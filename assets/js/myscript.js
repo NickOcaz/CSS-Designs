4873227bab7ebfdf327c800e092c51dee6567cf6
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2024-10-19 22:51:46 +0000

    java script update #2

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -2,3 +2,10 @@
 function test() {
     alert("I am not lying!");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("hoverDiv").onmouseover = function() { hoverFunction(); };
+});
+
+function hoverFunction() {
+    alert("You hovered over the div!");
+}
